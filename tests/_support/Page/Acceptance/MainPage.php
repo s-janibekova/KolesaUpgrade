d5ee7af89acdff277625af509d25cf28d2109391
sjanibekova <sdzhanybekova@seikolabs.kz>
2021-06-17 01:18:07 +0600

    asdf

--- tests/_support/Page/Acceptance/MainPage.php
+++ tests/_support/Page/Acceptance/MainPage.php
@@ -6,75 +6,53 @@ class MainPage
     // include url of current page
     public static $URL = '';
 
+
     /**
-     * Селектор товара  
+     * Селектор для катетогории Dresses в верхнем меню 
      */
-    public static $product = '//ul[@id="homefeatured"]//li[%d]';
+    public static $topMenuLiDresses = '//*[@id="block_top_menu"]/ul/li[2]/a';
 
-    /**
-     * Селекторв quick view товара 
+    /** 
+     * Селектор для Summer Dresses в каталоге dresses 
      */
-    public static $quickViewProduct = '//ul[@id="homefeatured"]//li[%d]//a[@class="quick-view"]';
+    public static $summerDressesLi = '//*[@id="block_top_menu"]/ul/li[2]/a';
 
-    /**
-    * IFrame класс 
-    */
-    public static $iFrameClass = '.fancybox-iframe';
-
-    /**
-     * Селектор добавления товара в избранное
-    */
-    public static $wishlistButton = '//*[@id="wishlist_button"]';
-
-    /**
-     * Селектор для закрытия плашки об успешном добавлении товара 
-     * в избранное
-     */
-
-    public static $closeSuceessAdd = "//a[@title='Close']";
-
-    /**
-    * Селектор закрытия окна quick View
-    */
-
-    public static $closeQuickView = "//a[@class='fancybox-item fancybox-close']";
     
-    /**
-     * Селектор кноки login на навигационном окне 
-     */
-    public static $loginLinkOnNav = '//a[@class="login"]';
-
 
     /**
-     * Селектор логоттипа  который возвращает в mainPage
-     */
-    public static $LogoOnHeader = "//div[@id='header_logo']";
-
-    /**
-     * Селектор для перехода на страницу аккаунта пользователя
-     */
-    public static $myAccountOnNav = "//div[@class='header_user_info']/a[@class='account']";
-
-    /**
-     * Селектор для логаута
+     * Проверяет видна ли категория Dresses в верхнем каталоге
      */
 
-     public static $logoutOnNav = '//*[@id="header"]//a[@class="logout"]';
+     public function isDressesLiSeen(){
 
-
-    /**
-     * Переход на wishlist пользователя 
-     */
-
-     public function goToMyAccount(){
-
-        $this->acceptanceTester->waitForElementVisible(MainPage::$myAccountOnNav);
-        $this->acceptanceTester->click(MainPage::$myAccountOnNav);
-
+        $this->acceptanceTester->seeElement(self::$topMenuLiDresses);
+        
         return $this;
      }
 
-    /**
+
+     /**
+      * Наводит мышью на категорию SummerDresses 
+      */
+
+      public function mouseOverSummerDressesLi(){
+
+       $this->acceptanceTester-> moveMouseOver(self::$summerDressesLi);
+
+       return $this;
+      }
+
+      /** 
+       * Кликает на категория SummerDresses
+       */
+      public function clickOnSummerDressesLi(){
+
+        $this->acceptanceTester->click(self::$summerDressesLi);
+
+      }
+
+
+        /**
      * @var \AcceptanceTester;
      */
     protected $acceptanceTester;
@@ -83,5 +61,13 @@ class MainPage
     {
         $this->acceptanceTester = $I;
     }
+
+
+
+    public static function route($param)
+    {
+        return static::$URL.$param;
+    }
+
 
 }
